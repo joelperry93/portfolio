a7e495092f61506a4dc29dcbd768d294c513ed1b
Joel Perry <joelperry@me.com>
2015-02-28 14:07:06 +0000

    Added project image boxes

--- index.php
+++ index.php
@@ -2,48 +2,46 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <meta charset="utf-8">
+    <title>joel perry's portfolio</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-  <!-- Basic Page Needs
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta charset="utf-8">
-  <title>joel perry's portfolio</title>
-  <meta name="description" content="">
-  <meta name="author" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <!-- Mobile Specific Metas
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
 
-  <!-- FONT
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/skeleton.css">
+    <link rel="stylesheet" href="css/style.css">
 
-  <!-- CSS
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="stylesheet" href="css/normalize.css">
-  <link rel="stylesheet" href="css/skeleton.css">
-   <link rel="stylesheet" href="css/style.css">
-
-  <!-- Favicon
-  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <link rel="icon" type="image/png" href="images/favicon.png">
+    <link rel="icon" type="image/png" href="images/favicon.png">
 </head>
 <body>
 
 <div class="container">
 	<div class="row">
-		<div class="one-half column" style="margin-top: 25%">
-			<h4>My name is Joel Perry and this my web development portfolio.</h4>
+        <div class="twelve column">
+            <header>
+      			<h4>Joel Perry's Web Development Portfolio.</h4>
+            </header>
 
-			<ul class="projects-list">
+			<ul class="projects-list row">
 				<?php foreach ($projects as $project): ?>
 					<a href="<?= $project->url ?>">
-						<li>
-              <span class="project-name"><?= $project->name ?></span>
-              <p class="web-technologies">
-                <?= implode(', ', $project->technologies) ?>
-              </p>
-            </li>
+						<li class="project six columns">
+                            <img src="<?= $project->image->url; ?>" class="project-preview-image" />
+                            
+                            <div class="row project-preview-footer">
+                                <div class="one-half column project-name">
+                                    <?= $project->name ?>
+                                </div>
+
+                                <p class="one-half column web-technologies">
+                                    <?= implode(', ', $project->technologies) ?>
+                                </p>
+                            </div>
+                        </li>
 					</a>
 				<?php endforeach; ?>
 			</ul>
