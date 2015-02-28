95ce528d0f931a56bc0a94491123bf5a4fede636
Joel Perry <joelperry@me.com>
2015-02-28 12:50:59 +0000

    Added some info and gitignore for p folder

--- index.php
+++ index.php
@@ -21,24 +21,39 @@
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="stylesheet" href="css/normalize.css">
   <link rel="stylesheet" href="css/skeleton.css">
+   <link rel="stylesheet" href="css/style.css">
 
   <!-- Favicon
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
   <link rel="icon" type="image/png" href="images/favicon.png">
+
 
 </head>
 <body>
 
   <!-- Primary Page Layout
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
-  <div class="container">
-    <div class="row">
-      <div class="one-half column" style="margin-top: 25%">
-        <h4>Basic Page</h4>
-        <p>This index.html page is a placeholder with the CSS, font and favicon. It's just waiting for you to add some content! If you need some help hit up the <a href="http://www.getskeleton.com">Skeleton documentation</a>.</p>
-      </div>
-    </div>
-  </div>
+<div class="container">
+	<div class="row">
+		<div class="one-half column" style="margin-top: 25%">
+			<h4>My name is Joel Perry and this my web development portfolio.</h4>
+
+			<ul>
+				<?php if ($handle = opendir('p/')): ?>
+					<?php while (false !== ($file = readdir($handle))): ?>
+					  	<?php if (substr($file, 0, 1) !== '.'): ?>
+							<a href="/p/<?php echo $file; ?>">
+								<li><?php echo $file; ?></li>
+							</a>
+					  	<?php endif; ?>
+					<?php endwhile; ?>
+
+					<?php closedir($handle); ?>
+				<?php endif; ?>
+			</ul>
+		</div>
+	</div>
+</div>
 
 <!-- End Document
   –––––––––––––––––––––––––––––––––––––––––––––––––– -->
